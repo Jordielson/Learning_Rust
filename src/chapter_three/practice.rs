17021e549fe1cf5354a7259a3e536fd9568b58a3
Jordielson <jordielsonemanuel@gmail.com>
2023-11-06 14:06:12 -0300

    pratice twelve days christmas

--- src/chapter_three/practice.rs
+++ src/chapter_three/practice.rs
@@ -52,3 +52,59 @@ pub fn generate_fibonacci_number(number: u32) -> BigUint {
 
     b
 }
+
+
+// Print the lyrics to the Christmas carol “The Twelve Days of Christmas,” 
+// taking advantage of the repetition in the song.
+pub fn print_twelve_days_christmas_lyrics() -> () {
+    for day in 0..12 {
+        let stanza = build_stanza_twelve_days_christmas_lyrics(day);
+        println!("{stanza}");
+    }
+}
+
+fn build_stanza_twelve_days_christmas_lyrics(stanza_number: usize) -> String {
+    let days: [&str; 12] = [
+        "first", "second", "third", "fourth", 
+        "fifth", "sixth", "seventh", "eighth",
+        "ninth", "tenth", "eleventh", "twelfth"
+    ];
+
+    let gifts: [&str; 11] = [
+        "Twelve drummers drumming,",
+        "Eleven pipers piping,",
+        "Ten lords a-leaping,",
+        "Nine ladies dancing,",
+        "Eight maids a-milking,",
+        "Seven swans a-swimming,",
+        "Six geese a-laying,",
+        "Five golden rings,",
+        "Four calling birds,",
+        "Three French hens,",
+        "Two turtle doves,",
+    ];
+
+    let mut stanza: String = format!(
+        "On the {} day of Christmas,\n", 
+        days[stanza_number]
+    );
+    stanza.push_str("My true love sent to me\n");
+
+    let mut count = stanza_number;
+    for gift in gifts {
+        if count == 0 {
+            break;
+        }
+        stanza.push_str(&format!("{}\n", gift));
+        count -= 1;
+    } 
+
+    stanza.push_str(
+        &format!("{} partridge in a pear tree.\n", 
+            if stanza_number == 1 { "A" }
+            else { "And a" }
+        ),
+    );
+
+    stanza
+}
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,5 @@ use crate::chapter_three::practice;
 pub mod chapter_three;
 
 fn main() {
-    let n = 5683;
-    let result = practice::generate_fibonacci_number(n);
-    println!("Fibonacci({}) = {}", n, result);
+    practice::print_twelve_days_christmas_lyrics();
 }
